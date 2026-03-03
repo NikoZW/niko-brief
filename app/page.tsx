25c9aa44c2f90093785ecbfc60557209b2085aff
Niko (nzzzzzw) <wzzclot@gmail.com>
2026-03-02 22:59:43 -0800

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { BookOpenCheck } from 'lucide-react';
+import Card from "@/components/Card";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-white">
+      <header className="flex items-center justify-between px-8 py-6 mx-w-7xl mx-auto">
+        <div className="flex items-center gap-3">
+          <BookOpenCheck />
+          <h1 className="text-2xl font-bold"> Daily News </h1>
+        </div>
+        <nav className="flex items-center gap-6">
+          <a href="/" className="hover:text-gray-500">Home</a>
+          <a href="/" className="hover:text-gray-500">Home</a>
+        </nav>
+      </header>
+      {/* main content */}
+      <div className="max-w-7xl mx-auto px-8 py-12">
+        {/* title */}
+        <div className="text-center mb-16">
+          <h2 className="text-5xl font-bold mb-6">
+            Daily Briefs By Niko
+          </h2>
+          <p className="text-lg text-gray-700 max-w-3xl mx-auto leading-relaxed">
+            Stay informed with Daily Briefs by Niko — concise, AI-powered news summaries covering global events, business, technology, and more, all in one place.
+          </p>
+        </div>
+        {/* input and suscrtibe button */}
+        <div className="text-center flex items-center justify-center gap-4">
+          <input type="email" placeholder="Enter your email" className="w-full max-w-md p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+          <button className="bg-black hover:bg-gray-800 text-white py-3 px-6 rounded-lg transition-colors cursor-pointer">
+            Subscribe
+          </button>
+        </div>
+        {/* Card */}
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <Card title="AI" description="Global stock markets rallied today as investors reacted positively to signs of economic recovery and strong corporate earnings reports." />
+          <Card title="Starups" description="Leading technology companies have unveiled their latest AI innovations, promising to revolutionize industries from healthcare to finance." />
+          <Card title="Technology" description="World leaders at the Climate Change Summit have reached new agreements aimed at reducing carbon emissions and combating global warming." />
+        </div>
+      </div>
+    </div>
+  );
+}
